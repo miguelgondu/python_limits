a89d3cacbd59f54fb1f9972fb7da34e138650546
Miguel González Duque <miguelgondu@gmail.com>
2017-07-04 21:59:42 -0500

    adopted a better naming convention for the functions

--- algorithm.py
+++ algorithm.py
@@ -1,19 +1,20 @@
+import math
 import sympy
 import random
-import math
 
 x = sympy.Symbol('x')
 y = sympy.Symbol('y')
 
 # Auxiliar functions
 
-def substituteInPoly(p, list_of_subst):
+def substitute_in_poly(p, list_of_subst):
     return p.as_expr().subs(list_of_subst, simultaneous = True).as_poly()
 
-def hgetter(f,g):
-    return x.as_poly()*(g*sympy.diff(f, x) - f*sympy.diff(g, x)) - y.as_poly()*(g*sympy.diff(f, y) - f*sympy.diff(g, y))
+def h_getter(f,g):
+    return (x.as_poly()*(g*sympy.diff(f, x) - f*sympy.diff(g, x))
+            - y.as_poly()*(g*sympy.diff(f, y) - f*sympy.diff(g, y)))
 
-def monicMaker(h):
+def monic_maker(h):
     '''
     This function peforms a rotation on a polynomial of two variables
     h(x,y). That is, it maps h(x,y) to h(x+ny, -nx + y)) for some n
@@ -30,16 +31,16 @@ def monicMaker(h):
     new_h = new_h * (1/sympy.LC(new_h, y))
     return new_h.as_poly()
 
-def newtonAutomorphism(h, q, p):
+def newton_automorphism(h, q, p):
     '''
     This function performs the Newton Automorphism (x maps to x ** q
     and y maps to y(x ** p)) to a polynomial h, returning a sympy expr
     (not a polynomial). Here, p and q are expected to be rational
     numbers.
     '''
-    return substituteInPoly(h, [(x, x ** q), (y, y*x**p)])
+    return substitute_in_poly(h, [(x, x ** q), (y, y*x**p)])
 
-def componentsOfy(F):
+def components_of_y(F):
     '''
     This function returns a list with the homogenous
     components of h as a polynomial in y in increasing order.
@@ -55,7 +56,7 @@ def componentsOfy(F):
         list_of_fs.append((1/math.factorial(j))*nextFprime.eval(y, 0).as_poly(x))
     return list_of_fs
 
-def componentsOfx(F):
+def components_of_x(F):
     '''
     This function returns a list with the homogenous
     components of h as a polynomial in y in increasing order.
@@ -71,7 +72,7 @@ def componentsOfx(F):
         list_of_fs.append((1/math.factorial(j))*nextFprime.eval(x, 0).as_poly(y))
     return list_of_fs
 
-def phiAutomorphism(h):
+def phi_automorphism(h):
     '''
     This function performs the phi automorphism to a polynomial that's monic in y,
     so that the term that's with degree(h, y) - 1 banishes. Also returns the 
@@ -79,14 +80,14 @@ def phiAutomorphism(h):
     '''
     x = sympy.Symbol('x')
     y = sympy.Symbol('y')
-    list_of_bs = componentsOfy(h)
+    list_of_bs = components_of_y(h)
     b1 = list_of_bs[-2]
     #print(sympy.degree(h, y))
     #print(h)
     #print(list_of_bs)
-    return substituteInPoly(h, [(y, y- (1/sympy.degree(h, y))*b1.as_expr())]), b1
+    return substitute_in_poly(h, [(y, y- (1/sympy.degree(h, y))*b1.as_expr())]), b1
 
-def leastDegreeInx(b):
+def least_degree_in_x(b):
     '''
     Returns the least degree in x of a polynomial b(x) (assumed to only be a 
     polynomial in x, not x and y).
@@ -100,16 +101,16 @@ def leastDegreeInx(b):
         k += 1
     return k
 
-def urgetter(h):
+def ur_getter(h):
     '''
     Given a polynomial 
         h(x,y) = y ** d + b_2(x)y**(d-2) + ... + b_d(x)
     this function returns the tuple (r, u_r) where u_r is the
     degree of b_r such that u_r/r is minimal.
     '''
-    list_of_bs = componentsOfy(h)
+    list_of_bs = components_of_y(h)
     list_of_bs = list_of_bs[::-1] # To follow the convention of the proof.
-    list_of_least_degrees = [leastDegreeInx(b) for b in list_of_bs[2:]] #Here, position 0 is really from b2.
+    list_of_least_degrees = [least_degree_in_x(b) for b in list_of_bs[2:]] #Here, position 0 is really from b2.
     list_of_quotients = [k/(i+2) for (i, k) in enumerate(list_of_least_degrees)]
     r_indexer = list_of_quotients.index(min(list_of_quotients))
     return r_indexer + 2, list_of_least_degrees[r_indexer]
--- tests.py
+++ tests.py
@@ -4,41 +4,41 @@ from algorithm import *
 x = sympy.Symbol('x')
 y = sympy.Symbol('y')
 
-def test_hgetter1():
+def test_h_getter1():
 	f = sympy.Poly(x, x, y)
 	g = sympy.Poly(y, x, y)
 
-	h = hgetter(f, g)
+	h = h_getter(f, g)
 	assert h == 2*x*y
 
-def test_hgetter2():
+def test_h_getter2():
 	f = sympy.Poly(x**2 + y**2)
 	g = sympy.Poly(2*y**3 + 4*x**2*y)
 
-	h = hgetter(f, g)
+	h = h_getter(f, g)
 	assert h == 4*x**4*y - 2*x**2*y**3 + 2*y**5
 
-def test_monicMaker():
+def test_monic_maker():
 	h = 4*x**4*y - 2*x**2*y**3 + 2*y**5
-	h1 = monicMaker(h)
+	h1 = monic_maker(h)
 	assert h1.as_poly(y).LC() == 1
 
 def test_phi_automorphism():
 	h = (y**2 - sympy.Rational(80,9)*x*y - x**2).as_poly(y)
-	h1, a = phiAutomorphism(h)
-	bs = componentsOfy(h1)
+	h1, a = phi_automorphism(h)
+	bs = components_of_y(h1)
 	assert bs[-2] == 0
 
 def test_newton_automorphism():
 	h = 2*x*y
-	new_h = newtonAutomorphism(h, sympy.Rational(1,2), sympy.Rational(1,3)) 
+	new_h = newton_automorphism(h, sympy.Rational(1,2), sympy.Rational(1,3)) 
 	assert new_h == 2*x**(sympy.Rational(5,6))*y
 
 def test_least_degree_in_x():
 	fx = x**2 + x
-	assert leastDegreeInx(fx) == 1
+	assert least_degree_in_x(fx) == 1
 
 def test_urgetter():
 	h = y**5 + (x**2+1)*y**3 + (2*x**3)*y**2 + (x+1)*y + x**3 + 2
-	r, ur = urgetter(h)
+	r, ur = ur_getter(h)
 	assert ur == 0 and r==2
